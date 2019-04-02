6dbbc8c26f20e9d76dc48dd1649a947c96e41210
BastyZ <bastian.inostroza@pm.me>
2019-04-02 16:33:55 -0300

    start of server

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,13 @@
-fun main() {
+import com.xenomachina.argparser.ArgParser
 
+class DnsServer(parser: ArgParser) {
+    val port by parser.storing("" +
+            "-p", "--port",
+        help="port number")
+}
+
+fun main(args: Array<String>) {
+    ArgParser(args).parseInto(::DnsServer).run {
+        print("DNS Server started, listening at port ${this.port}")
+    }
 }
